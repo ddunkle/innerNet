31a65da8ad9f886152c6e08da92df1094355961d
jrtwheeler <58869889+jrtwheeler@users.noreply.github.com>
2020-11-28 12:03:58 -0500

    Fixed signup page

--- public/js/signup.js
+++ public/js/signup.js
@@ -29,7 +29,7 @@ $(document).ready(() => {
       password: password
     })
       .then(() => {
-        window.location.replace("../../views/layouts/main");
+        window.location.replace("/members");
         // If there's an error, handle it by throwing up a bootstrap alert
       })
       .catch(handleLoginErr);
