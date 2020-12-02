15d13c9f52538c31e23aea690e2bfdb10fd6e656
ddunkle <47399617+ddunkle@users.noreply.github.com>
2020-12-02 18:56:51 -0500

    revised api route (#15)

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -50,8 +50,21 @@ module.exports = function(app) {
       });
     }
   });
+
+  app.post("/api/user_post", (req, res) => {
+    db.User.create({
+      email: req.body.email,
+      password: req.body.password
+    })
+      .then(() => {
+        res.redirect(307, "/api/login");
+      })
+      .catch(err => {
+        res.status(401).json(err);
+      });
+  });
+
 };
 
-app.get("/api/user_posts/:userID", (req,res) => {
-  
-})
+
+
