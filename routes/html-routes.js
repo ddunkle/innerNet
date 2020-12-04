7788559ddabc228038f5d28bac361019a40af8f0
Jim Wheeler <jwheele1@gmail.com>
2020-12-03 20:16:54 -0500

    Working login and signup (#20)

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -5,36 +5,36 @@ const db = require("../models");
 // Requiring our custom middleware for checking if a user is logged in
 const isAuthenticated = require("../config/middleware/isAuthenticated");
 
-module.exports = function(app) {
-  app.get("/", (req, res) => {
+module.exports = function (app) {
+  app.get("/", isAuthenticated, (req, res) => {
     // If the user already has an account send them to the members page
-    if (req.user) {
-      res.redirect("/members");
-    }
-    res.render("signup");
+      db.Post.findAll({
+        where: {
+          UserId: req.user.id
+        }
+      }).then(function (dbPost) {
+        res.render("home");
+      })
   });
 
   app.get("/login", (req, res) => {
     // If the user already has an account send them to the members page
     if (req.user) {
-      res.redirect("/members");
+      return res.redirect("/");
     }
     res.render("login");
+  });
+
+  app.get("/signup", (req, res) => {
+    if (req.user) {
+      return res.redirect("/");
+    }
+    res.render("signup");
   });
 
   // Here we've add our isAuthenticated middleware to this route.
   // If a user who is not logged in tries to access this route they will be redirected to the signup page
   app.get("/members", isAuthenticated, (req, res) => {
-      db.Post.findAll({
-        where: {
-          UserId: req.user.id
-        }
-      }).then(function(dbPost){
-        console.log(dbPost);
-        //res.json(dbPost);
-        res.render("home");
-      })
-
-    //res.render("home");
+    res.render("home");
   });
 };
