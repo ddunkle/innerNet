515c0537c26a94ad87a8c3068a99a0abf61dd04b
Jim Wheeler <jwheele1@gmail.com>
2020-12-04 20:09:56 -0500

    Working links (#23)

--- public/js/index.js
+++ public/js/index.js
@@ -2,12 +2,11 @@ $(document).ready(function () {
     //console.log("inside function");
     let todos = [];
     getTodos();
-    
+
 
     // This function grabs todos from the database and updates the view
     function getTodos() {
-        $.get("/api/items", function(data) {
-            //console.log("endpoint hit");
+        $.get("/api/items", function (data) {
             todos = data;
             addTodos(todos);
         });
@@ -15,35 +14,39 @@ $(document).ready(function () {
 
     function addTodos(todos) {
         console.log(todos);
-        for (var i = 0; i<todos.length; i++) {
+        for (var i = 0; i < todos.length; i++) {
             if (todos[i].isActive) {
-                // console.log(todos[i]);
-                var row = $("<div>");
+                let row = $("<div>");
                 row.addClass("row");
-                var col1 = $("<div>");
+                let col1 = $("<div>");
                 col1.addClass("col-md-10");
-                col1.text(todos[i].title);
-                var col2 = $("<div>");
+                let url = JSON.stringify(todos[i].title)
+                console.log(url)
+                url = url.slice(1, 5)
+                console.log(url)
+                if (JSON.stringify(todos[i].title).slice(1, 5) === "http") {
+                    let atagOne = $("<a>")
+                    atagOne.text(todos[i].title);
+                    atagOne.attr("href", todos[i].title);
+                    atagOne.attr("target","_blank");
+                    col1.append(atagOne);
+                } else {
+                    col1.text(todos[i].title);
+                }
+                let col2 = $("<div>");
                 col2.addClass("col-md-2");
-                var atag = $("<a>");
+                let atag = $("<a>");
                 atag.text("delete");
-                //atag.click(function() {runArchive(todos[i].id)});
-                //atag.attr("onclick", runArchive(todos[i].id))
-                var href = "/api/archive/" + todos[i].id;
+                let href = "/api/archive/" + todos[i].id;
                 atag.attr("href", href);
                 col2.append(atag);
                 row.append(col1, col2);
-                var category = todos[i].category
-                $(`#${category}`).append(row);            
+                let category = todos[i].category
+                $(`#${category}`).append(row);
             }
         }
     }
 
-    // function runArchive(id) {
-    //     $.put(`/api/archive/${id}`, function(data) {
-    //         console.log(data);
-    //     });
-    // }
 });
 
 
